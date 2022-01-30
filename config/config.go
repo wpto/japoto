04c56fb58c70a2cf039ffc090975f7573c4e7949
pgeowng <30204558+pgeowng@users.noreply.github.com>
2022-01-30 22:04:47 +0300

    add: show recent date

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 const Dest = "./public"
 const Static = "./static"
-const PublicURL = "https://pgeowng.github.io/japoto"
 const RecentLimit = 30
 const FileSourcePath = "../japoto-private/japoto.json"
-const ChannelPrefix = "https://t.me/japoto"
+const ChannelPrefix = "https://t.me/japoto/"
+const PublicURL = "https://pgeowng.github.io/japoto"
